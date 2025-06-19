862460801b59fd7fef37771c979aa7e2f47fe732
indra554 <indra_al_rasyid@teknokrat.ac.id>
2025-06-20 02:00:15 +0700

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alialias(libs.plugins.kotlin.android) apply falseas(libs.plugins.android.application) apply false
-
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
