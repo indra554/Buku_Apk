b5c82e81c93708bdd66205a7c1e55dfed47cd448
indra554 <indra_al_rasyid@teknokrat.ac.id>
2025-06-23 20:22:16 +0700

    commit

--- app/src/main/java/com/example/bukuapp/BookDetailActivity.kt
+++ app/src/main/java/com/example/bukuapp/BookDetailActivity.kt
@@ -31,7 +31,29 @@ class BookDetailActivity : AppCompatActivity() {
         book?.let { displayBookDetails(it) }
     }
 
+    private fun displayBookDetails(book: Book) {
+        // Load book image from drawable
+        findViewById<android.widget.ImageView>(R.id.bookImage).setImageResource(book.imageResId)
 
+        // Set book details
+        findViewById<android.widget.TextView>(R.id.bookTitle).text = book.title
+        findViewById<android.widget.TextView>(R.id.bookAuthor).text = book.author
+
+        // Format price in Indonesian Rupiah
+        val formatter = NumberFormat.getCurrencyInstance(Locale("id", "ID"))
+        findViewById<android.widget.TextView>(R.id.bookPrice).text = formatter.format(book.price)
+
+        findViewById<android.widget.TextView>(R.id.bookDescription).text = book.description
+
+        // Set up WhatsApp order button
+        findViewById<MaterialButton>(R.id.orderButton).setOnClickListener {
+            val message = "Saya ingin pesan buku ${book.title}"
+            val encodedMessage = Uri.encode(message)
+            val whatsappUrl = "https://wa.me/6281290104184?text=$encodedMessage"
+
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(whatsappUrl)
+            }
             startActivity(intent)
         }
     }
